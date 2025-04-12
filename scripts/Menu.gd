31a820abaf60b1f561110eee3856b9f56c3b31b9
Mac Zhang <lemac2002@gmail.com>
2025-04-12 14:25:36 -0400

    Fixed buttons
    Button fix

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -1,10 +1,10 @@
 extends Control
 
-func _on_play_pressed():
+func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Game.tscn")
 
-func _on_instructions_pressed():
-	pass#
+func _on_instructions_pressed() -> void:
+	pass # Replace with function body.
 
-func _on_exit_pressed():
+func _on_exit_pressed() -> void:
 	get_tree().quit()
