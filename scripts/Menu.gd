1c5c38742067e2446dc981c7667cc8052c9eb6a3
Mac Zhang <lemac2002@gmail.com>
2025-04-12 18:14:27 -0400

    Added menu textures
    menu textures

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -1,10 +1,12 @@
 extends Control
 
 func _on_play_pressed() -> void:
+	print("Play Pressed")
 	get_tree().change_scene_to_file("res://scenes/Game.tscn")
 
 func _on_instructions_pressed() -> void:
-	pass # Replace with function body.
+	print("Instructions Pressed")
+	get_tree().change_scene_to_file("res://scenes/Instructions.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
