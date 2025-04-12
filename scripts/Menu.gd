e52f2a018c259ac1604802167484cfd9d2f75ed6
Mac Zhang <lemac2002@gmail.com>
2025-04-12 14:13:20 -0400

    Assets + Menu
    Added initial assets and menu

---
+++ scripts/Menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/Game.tscn")
+
+func _on_instructions_pressed():
+	pass#
+
+func _on_exit_pressed():
+	get_tree().quit()
