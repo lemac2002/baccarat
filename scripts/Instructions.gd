998118b634e04b0b4b55ce5f67b0c6e329d65885
Mac Zhang <lemac2002@gmail.com>
2025-04-17 17:53:08 -0400

    Button Sounds
    Button Sounds

--- scripts/Instructions.gd
+++ scripts/Instructions.gd
@@ -2,4 +2,9 @@ extends Control
 
 
 func _on_back_pressed() -> void:
+	print("Instructions Pressed")
+	var sfx = AudioStreamPlayer.new()
+	sfx.stream = preload("res://assets/Sounds/Minimalist2.mp3")
+	add_child(sfx)
+	sfx.play()
 	get_tree().change_scene_to_file("res://scenes/Main.tscn")
